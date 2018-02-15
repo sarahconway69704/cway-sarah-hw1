cb7354fddacadc5adb78c91a2ecf6b5f2f416d29
Conway <s_conway69704@tss-hr420-dm12.local>
2018-02-15 10:32:34 -0500

    updates for hw2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 (() => {
-  console.log(Fired! This is Fun!);
+  console.log('fired!')
+
+//variables at the top
+
+var iconSet = document.querySelectorAll('svg');
+
+//functions here
+function logThisIcon(){
+  console.log('clicked on this icon: ', this.id);
+
+  this.style.opacity = 0.5;
 }
+
+
+  // event handling goes at the bottom
+
+  iconSet.forEach(icon => icon.addEventListener('click', logThisIcon))
+})();
